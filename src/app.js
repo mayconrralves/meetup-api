bef26e7d561c2f3dfb841c6dcd28e93c21a7224d
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2020-10-13 19:47:29 -0300

    Adicionado cors e captura de erros com express-async-errors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 import path from 'path';
-
+import cors from 'cors';
+import Youch from 'youch';
 import './database';
 
 class App {
@@ -10,14 +11,26 @@ class App {
         this.server = express();
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     middlewares() {
         this.server.use(express.json());
         this.server.use('/user/avatar', express.static(path.resolve(__dirname, '..', 'storage', 'uploads')));
     }
+
     routes() {
         this.server.use(routes);
+    }
+
+    exceptionHandler() {
+        this.server.use(async(err, req, res, next)=>{
+            if(process.env.NODE_ENV === 'development'){
+                const errors = await new Youch(err, req).toJSON();
+                return res.status(500).json(errors);
+            }
+            return res.status(500).json({error: "Internal server error"});
+        });
     }
 }
 
