4561c787876a438e13c79c8bdbe037b5c62e0a8c
Maycon Rogers Ribeiro Alves <mayconrralves@yahoo.com.br>
2020-11-20 08:01:50 -0300

    Added validation for files

---
+++ src/app/validations/FileValidation.js
@@ -0,0 +1,12 @@
+const exp = /(\.jpg$)|(\.jpeg$)|(\.gif$)|(\.png$)|(\.jfif$)|(\.svg$)/;
+export default (req, res, next) => {
+	
+	if(!req.file){
+		return res.status(400).json({ error:  "File cannot be empty"});
+	}
+	if(!req.file.originalname.match(exp)){
+		return res.status(400).json({error: "The file type isn't supported. Must be: png/jpg/gif/jpeg"});
+	}
+
+	next();
+}
